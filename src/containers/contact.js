37dd318a7d35480b0671b950c8bfae99e3250eb4
Thibaud Lamothe <thibaud.lamothe@gmail.com>
2020-12-03 08:26:29 +0100

    added contact formular (not yet linked)

--- src/containers/contact.js
+++ src/containers/contact.js
@@ -1,22 +1,51 @@
 import React from "react"
 import Bandeau from "../components/bandeau"
 
-import Container from 'react-bootstrap/Container'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
+import Container from 'react-bootstrap/Container'
+
 
 const Contact = () => (
-
-    <Bandeau title="Contact">
-        <Container>
-            <Row>
-                Formulaire.
-            </Row>
-            <Row >
-                <Col>Row2 - Col1</Col>
-                <Col>Row2 - Col2</Col>
-            </Row>
-        </Container>
+  
+    <Bandeau title="Get in touch">
+      <Container>
+        <Form>
+          <Row>
+            <Col sm className="p-2">          
+              <Form.Group controlId="exampleForm.ControlInput1">
+                <Form.Label>Username</Form.Label>
+                <Form.Control type="text" placeholder="Ex : Thibaud L." required/>
+              </Form.Group>
+            </Col>
+            <Col sm className="p-2">
+              <Form.Group controlId="exampleForm.ControlInput1">
+                <Form.Label>*E-mail adress </Form.Label>
+                <Form.Control type="email" placeholder="Ex : hello@etomal.com" required />
+              </Form.Group>
+            </Col>
+          </Row>
+          
+          <Row>
+            <Col className="p-2">
+              <Form.Group controlId="exampleForm.ControlTextarea1">
+                {/* <Form.Label>Your message</Form.Label> */}
+                <Form.Control as="textarea" rows="6" placeholder="Your message here 🤠"/>
+              </Form.Group>
+            </Col>
+          </Row>
+          
+          <Row className="ml-auto p-2">
+          <Button variant="primary"  type="submit" className="onTheRight">
+            Send
+          </Button>
+          </Row>
+        </Form> 
+      </Container>
     </Bandeau>
+ 
 )
+
 export default Contact
